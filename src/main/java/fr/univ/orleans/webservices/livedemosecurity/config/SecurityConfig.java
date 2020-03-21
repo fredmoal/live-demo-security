917070a2df26f023ae4bf8120e75cefe826b22a6
Frédéric Moal <frederic.moal@univ-orleans.fr>
2020-03-21 10:26:39 +0100

    remove unused code/import

--- src/main/java/fr/univ/orleans/webservices/livedemosecurity/config/SecurityConfig.java
+++ src/main/java/fr/univ/orleans/webservices/livedemosecurity/config/SecurityConfig.java
@@ -3,36 +3,19 @@ package fr.univ.orleans.webservices.livedemosecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-/*
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("fred").password("{noop}fred").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}admin").roles("USER","ADMIN");
-        {bcrypt} for BCryptPasswordEncoder,
-        {noop} for NoOpPasswordEncoder,
-        {pbkdf2} for Pbkdf2PasswordEncoder,
-        {scrypt} for SCryptPasswordEncoder,
-        {sha256} for StandardPasswordEncoder.
-    }
-*/
+
     @Bean
     @Override
     protected UserDetailsService userDetailsService() {
